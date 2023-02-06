4f4185565a7f8ba162e2d27822d405cf61d2121e
lucas oliveira <https://github.com/lucasoliver-dev/quiz-familia>
2023-02-06 17:37:58 -0300

    app

---
+++ src/data/questions_complete.js
@@ -0,0 +1,93 @@
+const data = [
+    {
+      category: "Vivian",
+      questions: [
+        {
+          question: "Qual o Nome do gato da Vivi?",
+          options: ["Chin", "Jhin", "Green", "Gin","Lucas"],
+          answer: "Gin",
+          tip: "Ele é fofinho",
+        },
+        {
+          question: "Qual o Nome da empresa da Vivi?",
+          options: ["Master", "Medclub", "MedSystem", "Apple"],
+          answer: "MedSystem",
+          tip: "Fica na Rua James Joule",
+        },
+        {
+          question: "Quantos Anos a Vivi fará em 2023 ?",
+          options: ["24", "25", "26", "27"],
+          answer: "26",
+        },
+        {
+          question: "Qual o mês do aniversário da Vivi?",
+          options: ["Março", "Abril", "Maio", "Junho"],
+          answer: "Maio",
+        },
+        {
+          question: "Qual é a cor preferida da Vivi ?",
+          options: ["Amarelo", "Roxo", "Preto", "Não sei"],
+          answer: "Não sei",
+        },
+      ],
+    },
+    {
+      category: "Claudia/Dom",
+      questions: [
+        {
+          question: "Qual a comida preferida da Claudia?",
+          options: ["Hambúrguer", "Pizza", "Costela", "Ela é Fitness"],
+          answer: "Ela é Fitness",
+          tip: "está de dieta",
+        },
+        {
+          question: "Qual o Nome do Filho do Dom ?:",
+          options: ["Marcus", "Barco", "Marco", "Narcos"],
+          answer: "Marco",
+        },
+        {
+          question: "Oque o casal Claudia e Dom mais gostam de fazer no lazer ?",
+          options: ["Cinema", "Passeio de lancha", "Restaurante", "Assistir série"],
+          answer: "Assistir série",
+        },
+      ],
+    },
+    {
+      category: "Família ",
+      questions: [
+        {
+          question: "Quem tem dois cachorrinhos da raça Lhasa Apso?",
+          options: [
+            "Vovó Sônia",
+            "Maurício e Valéria",
+            "Biel e esposa",
+            "Rafa",
+          ],
+          answer: "Biel e esposa",
+        },
+        {
+          question: "Qual é o nome da namorada do Nathan?",
+          options: ["Fabiana", "Adriana", "Mariana", "Nenhuma"],
+          answer: "Nenhuma",
+        },
+        {
+          question: "Qual a cidade da segunda casa do Tio Maurício/Valéria?",
+          options: ["Macapá", "Dubai", "osasco", "Mairiporã"],
+          answer: "Mairiporã",
+        },
+        {
+          question: "Qual o nome do bairro da vovó sônia?",
+          options: ["Alphaville", "Santana", "Vila madalena", "Vila guilhermina"],
+          answer: "Santana",
+        },
+        {
+          question:
+            "Qual destas pessoas votou em Bolsonaro em 2022?",
+          options: ["Maurício", "Vivi", "Lula", "Dilma"],
+          answer: "Maurício",
+        },
+      ],
+    },
+  ];
+  
+  export default data;
